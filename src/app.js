bc7f2c499e1078922a6858f169a0f996b6626f28
bablu achra <babluachra@gmail.com>
2025-02-14 22:18:03 +0530

    controller and routes work

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
- constants.js index.js
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentialsl: true
+}));
+
+//  three configuration
+//  form data 
+app.use(express.json({limit:"16kb"}))
+//  configuration for url data
+app.use(express.urlencoded({extended:true, limit:'16kb'}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// routes
+import userRouter from './routes/user.routes.js'
+
+
+
+// routes deceleration
+
+app.use('/api/v1/users',userRouter)
+
+//  https://localhost:8000/api/v1/users/register
+export {app}

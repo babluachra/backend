431c1ffa25917b551b02370083cdc380ce42aba3
bablu achra <babluachra@gmail.com>
2025-02-14 12:37:14 +0530

    mongoose connection complete

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# backend series 
+
+this is a full backend project
--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-ECHO is on.
+// require('dotenv').config({path:'./env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config(
+    {path:'./env'}
+)
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// IIFE
+/*
+import express from "express";
+const app= express();
+
+;(async ()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       app.on("error",(error)=>{
+            console.log("ERROR", error);
+            throw error
+       })
+       app.listen(process.env.PORT,()=>{
+            console.log(`your sevrer is listening at port${process.env.PORT} `)
+       })
+    } catch (error) {
+        console.error("ERROR", error);
+        throw err
+    }
+})()
+*/
+
+
+
+
+
+// approach 1
+// function connectDb(){}
+// connectDb()
